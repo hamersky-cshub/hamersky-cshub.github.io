9147ee917b32d83516dae9d6f914cff1d9edcdc5
EclecticCyber <hamersky@cshub.cz>
2025-05-29 13:18:50 +0200

    First push

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import tailwind from '@astrojs/tailwind'
+import compress from 'astro-compress'
+import icon from 'astro-icon'
+
+// https://astro.build/config
+export default defineConfig({
+  compressHTML: true,
+  site: 'https://hamersky-cshub.github.io',
+  base: 'https://github.com/hamersky-cshub/hamersky-cshub.github.io',
+  integrations: [
+    mdx(),
+    icon(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    compress(),
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          logger: {
+            warn: () => {},
+          },
+        },
+      },
+    },
+  },
+})
