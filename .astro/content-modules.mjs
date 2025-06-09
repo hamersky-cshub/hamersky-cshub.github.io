e728431b8e7144e4ad52fa8022477d96d2d1336c
EclecticCyber <hamersky@cshub.cz>
2025-06-09 14:20:21 +0200

    Added missing module

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,10 +1,1 @@
-
-export default new Map([
-["src/content/projects/project-02.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fproject-02.mdx&astroContentModuleFlag=true")],
-["src/content/projects/project-01.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fproject-01.mdx&astroContentModuleFlag=true")],
-["src/content/projects/project-04.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fproject-04.mdx&astroContentModuleFlag=true")],
-["src/content/projects/project-03.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fproject-03.mdx&astroContentModuleFlag=true")],
-["src/content/projects/project-06.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fproject-06.mdx&astroContentModuleFlag=true")],
-["src/content/projects/project-07.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fproject-07.mdx&astroContentModuleFlag=true")],
-["src/content/projects/project-05.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fproject-05.mdx&astroContentModuleFlag=true")]]);
-		
+export default new Map();
