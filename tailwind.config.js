54055307f2bb25f028a4cf40fdc4b1e2f4db6dbd
EclecticCyber <hamersky@cshub.cz>
2025-08-07 13:57:39 +0200

    Improving typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  fontFamily: {
+  londrina: [
+    "Londrina Solid",
+    "sans-serif",
+  ],
+},
   theme: {
     extend: {},
   },
